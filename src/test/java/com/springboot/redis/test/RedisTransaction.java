f5f5208b6e3de44f65eca2f8bf205c0c6e13ae5d
gxing <gxing19@163.com>
2018-11-07 15:54:09 +0800

    删除无效依赖

--- src/test/java/com/springboot/redis/test/RedisTransaction.java
+++ src/test/java/com/springboot/redis/test/RedisTransaction.java
@@ -1,17 +1,13 @@
 package com.springboot.redis.test;
 
 import com.alibaba.fastjson.JSON;
-import com.springboot.redis.config.RedisConfig;
-import io.lettuce.core.api.async.RedisTransactionalAsyncCommands;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
